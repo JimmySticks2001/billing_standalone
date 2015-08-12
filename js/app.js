57fe765a97c92b507005fa71709f39b82080f10b
Tim Lightfoot <tlightfo@gmail.com>
2015-08-12 10:50:18 -0500

    Commit before branch

--- js/app.js
+++ js/app.js
@@ -16,8 +16,9 @@ win.showDevTools();
 // get all of the dot html templates
 var viewPartials = require('dot').process({ path: "./views"});
 
-$("#page").html(viewPartials.login);
-
+// require xlsx module so we can read Excel files.
+if(typeof require !== 'undefined') XLSX = require('xlsx');
+$("#page").html(viewPartials.upload);
 
 //Function to open up a notification on the top of the page. Type is a string: alert, success, or info.
 // Message is a string representing the message you want the user to see.
@@ -27,7 +28,7 @@ function Notification(type, message) {
   //make a new one. Pretty much restarting the timer.
   timeoutID = window.setTimeout(function(){$(".notice").slideUp(250)},5000);
   $(".notice").html(viewPartials.notice({type: type, message: message})).slideDown(250);
-}
+}//end Notification
 
 
 //main javascript bullshit goes here
@@ -56,30 +57,25 @@ $(document).ready(function() {
     win.minimize();
   });
 
-  //if the log in button is clicked
-  $("body").on('click', '#success', function(){
-    Notification("success", "Something has been successful");
+  //if the log in menu button is clicked
+  $("body").on('click', '#menu_login', function(){
+    $("#page").html(viewPartials.login);
+    componentHandler.registerElement("body");
   });
 
-  //if the log in button is clicked
-  $("body").on('click', '#alert', function(){
-    Notification("alert", "This is an alert");
+  //if the upload 837 menu button is clicked
+  $("body").on('click', '#menu_upload_837', function(){
+    $("#page").html(viewPartials.upload);
   });
-
-  //if the log in button is clicked
-  $("body").on('click', '#info', function(){
-    Notification("info", "This is informative");
-  });
-
 
 });//end ready
 
 
+// Events called when the window is maximized or minimized so we can change the header window icons.
 win.on('unmaximize', function(){
   windowMaximized = false;
   $("#window_max").text("crop_landscape");
 });
-
 win.on('maximize', function(){
   windowMaximized = true;
   $("#window_max").text("filter_none");
